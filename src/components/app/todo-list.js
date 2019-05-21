07f7b7c752f66db8d9a7adc13b0079b18f83490f
subelta <40162440+subelta@users.noreply.github.com>
2019-05-21 21:09:15 +0300

    filter completed n active

--- src/components/app/todo-list.js
+++ src/components/app/todo-list.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import Island, { AdaptiveIsland, Header, Content } from '@jetbrains/ring-ui/components/island/island';
-import { Tabs, Tab, SmartTabs, CustomItem } from '@jetbrains/ring-ui/components/tabs/tabs';
+// import { Tabs, Tab, SmartTabs, CustomItem } from '@jetbrains/ring-ui/components/tabs/tabs';
+import Group from '@jetbrains/ring-ui/components/group/group';
+import Button from '@jetbrains/ring-ui/components/button/button';
+
 
 import './todo-list.css'
 import Todo from './todo'
@@ -14,11 +17,15 @@ class TodoList extends Component {
         super(props);
         this.state = {
             todos : [],
+            tab: 'all'
         }
         this.create = this.create.bind(this);
         this.remove = this.remove.bind(this);
         this.update = this.update.bind(this);
         this.toggleCompletion = this.toggleCompletion.bind(this);
+        this.showAll = this.showAll.bind(this);
+        this.showActive = this.showActive.bind(this);
+        this.showCompleted = this.showCompleted.bind(this);
     }
 
 
@@ -26,7 +33,6 @@ class TodoList extends Component {
         if (localStorage.hasOwnProperty('todos')) {
             if (localStorage.getItem('todos').length > 2) {
                 // Читаем из нашего localstorage
-                console.log(localStorage.getItem('todos'));
                 this.getFromLocalStorage();
             } else {
                 // Читаем из нашего JSONа
@@ -35,7 +41,6 @@ class TodoList extends Component {
                     return todo;
                 })
                 this.setState({ todos : data});
-                console.log("srbtefvdAS");
             }
         } 
     
@@ -95,9 +100,26 @@ class TodoList extends Component {
         this.setState({todos: updatedTodos});
     }
 
+
+    showAll() {
+        this.setState({
+            tab : "all"
+        })
+    }
+    
+    showActive() {
+        this.setState({
+            tab : "active"
+        })
+    }
+
+    showCompleted() {
+        this.setState({
+            tab : "completed"
+        })
+    }
+
     render() {
-        // console.log(this.state.todos)
-        // localStorage.setItem('todos', JSON.stringify(this.state.todos));
         const todos = this.state.todos.map(todo => {
             return <Todo 
                         key={todo.id} 
@@ -109,17 +131,44 @@ class TodoList extends Component {
                         toggleTodo={this.toggleCompletion}
                     />;
         });
+        
+        const completed = todos.filter(todo => todo.props.completed);
+        const active = todos.filter(todo => !todo.props.completed);
+        const tab = this.state.tab;
+
         return(
             <Island className="limited-island" narrow>
                 <Header className="island-header" border>
                     <h1 className="heading">Todo List</h1>
                     <p className="info">With React and Ring UI</p>
                     <NewTodo createTodo={this.create}/>
-                    {/* <Tabs></Tabs> */}
+                    <Group>
+                        <Button 
+                            onClick={this.showAll} 
+                            active={tab == 'all'} 
+                            name="all" 
+                            title="Show all">
+                            Show all
+                        </Button>
+                        <Button 
+                            onClick={this.showActive} 
+                            active={tab == 'active'} 
+                            name="active" 
+                            title="Show active">
+                            Show active
+                        </Button>
+                        <Button 
+                            onClick={this.showCompleted} 
+                            active={tab == 'completed'} 
+                            name="completed" 
+                            title="Show completed">
+                            Show completed
+                        </Button>
+                    </Group>
                 </Header>
-                <Content fade>
+                <Content className="island-content" fade>
                     <ul>
-                        { todos }
+                        { (tab == "all") ? todos : (tab == "active") ? active : completed }
                     </ul>
                 </Content>
             </Island>
